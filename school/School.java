81ddc6c6cc0da5f1f24bdfb4ff47935e65a872b5
youenn-denece <youenn.denece@gmail.com>
2022-11-09 11:34:44 +0100

    test getStudentByName in Main

--- school/School.java
+++ school/School.java
@@ -9,12 +9,13 @@ public class School {
         TheSchool s1 = new TheSchool(new ArrayList<Person>(), null);
 
         s1.addPerson(new Student("Toto", "CE2", 3.5));
-       // s1.addPerson(new Director("Mr Le Directeur"));
+        s1.addPerson(new Director("Mr Le Directeur"));
         s1.sayHello();
 
-        s1.getStudentByName("Toto").setNote(5.5);;
-
+        s1.getStudentByName("Toto").setNote(5.5);
         s1.sayHello();
 
+        s1.getStudentByName("Toto").setName("Titi");
+        s1.sayHello();
     }
 }
