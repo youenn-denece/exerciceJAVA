45c8d0ff9a8f9668337dc72380f2a3f513d16edf
youenn-denece <youenn.denece@gmail.com>
2022-11-15 14:02:50 +0100

    test package

---
+++ testPackage/TestPack.java
@@ -0,0 +1,11 @@
+import myLib.*;
+
+public class TestPack {
+    public static void main(String[] args)
+    {
+        myLib.ClasseA ka = new myLib.ClasseA();
+
+        System.out.println(  ka  );
+        // je suis la classe A 
+    }        
+}
---
+++ testPackage/myLib/ClasseA.java
@@ -0,0 +1,12 @@
+package myLib;
+
+public class ClasseA {
+
+    @Override
+    public String toString() {
+        return "je suis la classe A ";
+    }
+
+
+    
+}
